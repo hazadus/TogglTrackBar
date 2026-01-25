7439520d9aae645ee8719cab804f39b4311c0491
hazadus <hazadus7@gmail.com>
2026-01-25 18:30:53 +0300

    Initial Commit

---
+++ TogglTrackBar/TogglTrackBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogglTrackBarApp.swift
+//  TogglTrackBar
+//
+//  Created by Alexander Goldovsky on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TogglTrackBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
